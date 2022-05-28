525ee6d4360cecf3caa69e83235853d2b4071164
Isaiah <greatpup1940@gmail.com>
2022-05-28 14:22:36 -0400

    API - Updating households by id

--- services/api/src/api/routes/household/controllers.ts
+++ services/api/src/api/routes/household/controllers.ts
@@ -1,5 +1,16 @@
-export const updateHousehold = (req:any,res:any) => {
-    res.send('household updated')
+import { Household } from "../../../models/Household"
+
+export const updateHousehold = async (req:any,res:any) => {
+
+    const { id } = req.params
+
+    try {
+        let updatedHousehold = await Household.findByIdAndUpdate(id, req.body)
+
+        res.status(200).json(updatedHousehold)
+    } catch (error) {
+        res.status(400).json(error)
+    }
 }
 
 export const addMember = (req:any,res:any) => {
