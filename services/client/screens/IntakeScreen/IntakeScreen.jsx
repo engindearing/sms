f7537a133c091316a8850cc5b076e06b2b98f7a2
Isaiah <greatpup1940@gmail.com>
2022-05-28 04:44:21 -0400

    Anti Discrimination

--- services/client/screens/IntakeScreen/IntakeScreen.jsx
+++ services/client/screens/IntakeScreen/IntakeScreen.jsx
@@ -7,7 +7,7 @@ import IntakeForm from "./IntakeForm";
 import Navigation from "./Navivgation";
 
 const IntakeScreen = ({ route }) => {
-  const [step, setStep, nextStep, prevStep] = useStep(21);
+  const [step, setStep, nextStep, prevStep] = useStep(22);
 
   const props = {
     step,
