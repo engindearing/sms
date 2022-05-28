e188fe5e76283bb2ce7f6357ce974c309778ad95
Isaiah <greatpup1940@gmail.com>
2022-05-28 14:38:12 -0400

    API - Bulk deleting household members

--- services/api/src/api/routes/household/controllers.ts
+++ services/api/src/api/routes/household/controllers.ts
@@ -1,35 +1,43 @@
-import { Household } from "../../../models/Household"
+import { Household } from "../../../models/Household";
 
-import { Member } from "../../../models/Member"
+import { Member } from "../../../models/Member";
 
-export const updateHousehold = async (req:any,res:any) => {
+export const updateHousehold = async (req: any, res: any) => {
+  const { id } = req.params;
 
-    const { id } = req.params
+  try {
+    let updatedHousehold = await Household.findByIdAndUpdate(id, req.body);
 
-    try {
-        let updatedHousehold = await Household.findByIdAndUpdate(id, req.body)
+    res.status(200).json(updatedHousehold);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 
-        res.status(200).json(updatedHousehold)
-    } catch (error) {
-        res.status(400).json(error)
-    }
-}
+export const addMembers = async (req: any, res: any) => {
+  const { id } = req.params;
 
-export const addMembers = async (req:any,res:any) => {
-    const { id } = req.params
+  let membersData = req.body.map((mem: any) => ({ ...mem, household: id }));
 
-    let membersData = req.body.map((mem:any) => ({ ...mem, household: id }))
+  try {
+    let members = await Member.create(membersData);
 
-    try {
+    res.json(members);
+  } catch (error) {
+    res.json(error);
+  }
+};
 
-        let members = await Member.create(membersData)
+export const deleteMembers = async (req: any, res: any) => {
+  const { id } = req.params;
 
-        res.json(members)
-        
-    } catch (error) {
+  let memberIds = req.body;
 
-        res.json(error)
-        
-    }
-}
+  try {
+    memberIds.map(async (id: string) => await Member.findByIdAndDelete(id));
 
+    res.status(204);
+  } catch (error) {
+    res.json(error);
+  }
+};
