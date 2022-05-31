4105703e99dc46d25c299ab10b0b26f1bc0b5a31
Isaiah <greatpup1940@gmail.com>
2022-05-31 14:49:19 -0400

    Heroku

--- services/api/src/utils/firebase.ts
+++ services/api/src/utils/firebase.ts
@@ -1,7 +1,15 @@
 import admin from "firebase-admin";
 
+import * as config from "./config.json";
+
+console.log(config);
+
 const firebase = admin.initializeApp({
-  credential: admin.credential.cert(require("./config.json")),
+  credential: admin.credential.cert({
+    projectId: config.project_id, // I get no error here
+    clientEmail: config.client_email, // I get no error here
+    privateKey: config.private_key.replace(/\\n/g, "\n"), // NOW THIS WORKS!!!
+  }),
   databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
