ebf1b115dffc775b303d2baf8ddcd3b936d2fb85
Isaiah <greatpup1940@gmail.com>
2022-06-04 16:43:41 -0400

    Insurance

--- services/client/screens/GuestScreen/components/Screens/Profile/Forms/Insurance.tsx
+++ services/client/screens/GuestScreen/components/Screens/Profile/Forms/Insurance.tsx
@@ -1,12 +1,119 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View } from "react-native";
 
-const Insurance = () => {
+import React from "react";
+
+import { useFormik } from "formik";
+
+import TextInput from "../../../../../../components/TextInput";
+
+import { Button, Checkbox, Text } from "native-base";
+
+import styled from "styled-components/native";
+
+import * as Yup from "yup";
+
+import "yup-phone";
+
+import { useDispatch, useSelector } from "react-redux";
+import { updateHouseholdById } from "../../../../../../state/slices/householdSlice";
+
+export default function Insurance({ navigation }) {
+  const { household } = useSelector((state: any) => state.household);
+
+  const dispatch = useDispatch();
+
+  const {
+    handleChange,
+    handleSubmit,
+    handleBlur,
+    errors,
+    touched,
+    values,
+    setFieldValue,
+  } = useFormik({
+    initialValues: {
+      insurance: {
+        hasInsurance: household.insurance.hasInsurance,
+        healthInsuranceType: household.insurance.healthInsuranceType,
+        membersCovered: household.insurance.membersCovered,
+      },
+    },
+    validationSchema: ContactSchema,
+
+    onSubmit: async (insurance) => {
+      dispatch(
+        updateHouseholdById({ householdId: household._id, payload: insurance })
+      );
+
+      navigation.navigate("Profile");
+    },
+  });
+
   return (
-    <View>
-      <Text>Insurance</Text>
+    <View style={{ padding: 10, height: "100%" }}>
+      <Checkbox
+        value="hasInsurance"
+        defaultIsChecked={values.insurance.hasInsurance}
+        onChange={(e) => setFieldValue("insurance.hasInsurance", e)}
+      >
+        Do you have health insurance?
+      </Checkbox>
+
+      <Spacer />
+
+      {values.insurance.hasInsurance && (
+        <>
+          <TextInput
+            width="100%"
+            placeholder="source"
+            onChangeText={handleChange("insurance.healthInsuranceType")}
+            name="insuranceType"
+            onBlur={handleBlur("insurance.healthInsuranceType")}
+            error={errors.insurance?.healthInsuranceType}
+            touched={touched.insurance?.healthInsuranceType}
+            value={values.insurance?.healthInsuranceType}
+            label="Health insurance source"
+          />
+
+          <TextInput
+            width="100%"
+            placeholder="covered"
+            onChangeText={handleChange("insurance.membersCovered")}
+            name="membersCovered"
+            onBlur={handleBlur("insurance.membersCovered")}
+            error={errors.insurance?.membersCovered}
+            touched={touched.insurance?.membersCovered}
+            value={values.insurance?.membersCovered}
+            label="Household Members covered"
+          />
+        </>
+      )}
+
+      <Button
+        style={{ position: "absolute", bottom: 5, width: "96%" }}
+        onPress={() => handleSubmit()}
+      >
+        Update
+      </Button>
     </View>
-  )
+  );
 }
 
-export default Insurance
+const ContactSchema = Yup.object().shape({
+  insurance: Yup.object().shape({
+    hasInsurance: Yup.boolean(),
+
+    healthInsuranceType: Yup.string(),
+
+    membersCovered: Yup.number()
+      .typeError("Please type in a number")
+      .positive("Must be greater than zero")
+      .min(0),
+  }),
+});
+
+const Spacer = styled.View`
+  margin-top: 2%;
+
+  margin-bottom: 2%;
+`;
