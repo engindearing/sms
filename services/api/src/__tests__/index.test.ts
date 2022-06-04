e65350cce4293b8455fa96bd2a69af70d40ff28f
Isaiah <greatpup1940@gmail.com>
2022-06-04 13:23:32 -0400

    Creating throwaway database when testing

--- services/api/src/__tests__/index.test.ts
+++ services/api/src/__tests__/index.test.ts
@@ -3,7 +3,7 @@ import supertest from "supertest";
 import app from "../api/app";
 
 describe("index", () => {
-  it("should return status code 200", async () => {
+  it("returns status code 200", async () => {
     await supertest(app).get("/api").expect(200);
   });
 });
