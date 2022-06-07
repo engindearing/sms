5ff016519581a38f6e39830d83432b4e8c83e009
Isaiah <greatpup1940@gmail.com>
2022-06-07 19:58:56 -0400

    Post inatall script

--- services/client/auth/axiosWithAuth.js
+++ services/client/auth/axiosWithAuth.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { NATIVE_API_URI } from "@env";
 
 export const axiosWithAuth = (accessToken) => {
+  alert(NATIVE_API_URI)
   return axios.create({
     headers: {
       authorization: `Bearer ${accessToken}`,
