b5880b26dad5507c3e89905168ec329fdb60bbe7
Isaiah <isaiahjfowler7@gmail.com>
2022-08-08 11:32:16 -0400

    Using env variable to specify server url

--- services/client/auth/axiosWithAuth.js
+++ services/client/auth/axiosWithAuth.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+import { NATIVE_API_URI } from "@env"
+
 export const axiosWithAuth = (accessToken) => {
   return axios.create({
     headers: {
       authorization: `Bearer ${accessToken}`,
     },
-    baseURL: `http://192.168.1.50:8000/api`,
+    baseURL: `${NATIVE_API_URI}/api` || null,
   });
 };
