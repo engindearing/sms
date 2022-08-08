f4184de265031d0386374e61c138cf5907c6256c
Isaiah Fowler <isaiahfowler@Isaiahs-MacBook-Pro.local>
2022-08-08 10:29:30 -0400

    bumped up node version, latest

--- services/client/auth/axiosWithAuth.js
+++ services/client/auth/axiosWithAuth.js
@@ -5,6 +5,6 @@ export const axiosWithAuth = (accessToken) => {
     headers: {
       authorization: `Bearer ${accessToken}`,
     },
-    baseURL: `http://192.168.1.49:8000/api`,
+    baseURL: `http://192.168.1.50:8000/api`,
   });
 };
